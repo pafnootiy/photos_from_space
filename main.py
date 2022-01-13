822188d53d811ed2fa135cd07f5c7c9c773794e6
pafnootiy <ksander-86@mail.ru>
2022-01-14 00:49:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests
-from pathlib import Path
-import os
 import datetime
+import os
+from pathlib import Path
+
+import requests
 from dotenv import load_dotenv
 
 
@@ -16,7 +17,7 @@ def get_image_url(url):
 
 
 def fetch_spacex_last_launch(url):
-    Path('C:/Users/Alex K/p_p/space_photos/images').mkdir(parents=True, exist_ok=True)
+    Path('photos_from_space/images').mkdir(parents=True, exist_ok=True)
     for numbers, link in enumerate(get_image_url(url)):
         filename = Path("images", f'spacex{numbers}.jpeg')
         response = requests.get(link)
@@ -25,10 +26,16 @@ def fetch_spacex_last_launch(url):
             file.write(response.content)
 
 
-def get_url_extension(nasa_url):
-    response = requests.get(nasa_url)
+
+
+def get_url_extension(apod_url):
+    api_key = os.getenv("API_KEY")
+    payload = {
+        "api_key": api_key,
+    }
+    response = requests.get(apod_url, params=payload)
     extension = os.path.splitext(response.json()['hdurl'])
-    return extension[1]
+    return print(extension[1])
 
 
 def get_apod(apod_url):
@@ -37,16 +44,17 @@ def get_apod(apod_url):
         "api_key": api_key,
         "count": 15
     }
+
     response = requests.get(apod_url, params=payload)
     response = response.json()
     apod_photo_links = []
-    for num, photo_links in enumerate(response):
+    for photo_links in response:
         apod_photo_links.append(photo_links['url'])
     return apod_photo_links
 
 
 def get_apod_photo(apod_url):
-    Path('C:/Users/Alex K/p_p/space_photos/apod_pics').mkdir(parents=True, exist_ok=True)
+    Path('photos_from_space/apod_pics').mkdir(parents=True, exist_ok=True)
     for numbers, link in enumerate(get_apod(apod_url)):
         filename = Path("apod_pics", f'apod_pics{numbers}.jpeg')
         response = requests.get(link)
@@ -58,24 +66,25 @@ def get_apod_photo(apod_url):
 def get_epic_link(epic_link):
     api_key = os.getenv("API_KEY")
     payload = {
-        "api_key": api_key,
-        "enhanced/date": "2021-06-23"
+        "api_key": api_key
     }
     response = requests.get(epic_link, params=payload)
     response = response.json()
-    date_for_link = response[0]["date"]
-    date = datetime.datetime.fromisoformat(date_for_link)
+    date_link = response[0]["date"]
+    image_name = response[0]["image"]
+    date = datetime.datetime.fromisoformat(date_link)
+    epic_photo_link = f"https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/{date.day}/png/{image_name}.png?api_key=o0LD7Oi1sBhhKp2hJVSiuiIj1G2gZNh4SZRxG537"
     epic_photo_link_list = []
 
     for image in response:
         epic_photo_link = f"https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/" \
-                          f"{date.day}/png/{image['image']}.png?api_key=DEMO_KEY"
+                              f"{date.day}/png/{image['image']}.png?api_key=o0LD7Oi1sBhhKp2hJVSiuiIj1G2gZNh4SZRxG537"
         epic_photo_link_list.append(epic_photo_link)
     return epic_photo_link_list
 
 
 def get_epic_photo(epic_link):
-    Path('C:/Users/Alex K/p_p/space_photos/epic_pics').mkdir(parents=True, exist_ok=True)
+    Path('photos_from_space/epic_pics').mkdir(parents=True, exist_ok=True)
     for number, links in enumerate(get_epic_link(epic_link)):
         filename = Path("epic_pics", f'epic_pics{number}.jpeg')
         response = requests.get(links)
@@ -87,17 +96,15 @@ def get_epic_photo(epic_link):
 def main():
     load_dotenv()
     url = 'https://api.spacexdata.com/v3/launches/'
-    nasa_url = "https://api.nasa.gov/planetary/apod?api_key=pi89FIExpKRQvAQgJYMTfzLCoIVyGHjQtVT1WR4K"
     apod_url = "https://api.nasa.gov/planetary/apod"
-    epic_link = "https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY"
-    get_image_url(url)
-    fetch_spacex_last_launch(url)
-    get_url_extension(nasa_url)
-    get_apod(apod_url)
-    get_apod_photo(apod_url)
-    get_epic_link(epic_link)
-    get_epic_photo(epic_link)
-
+    epic_link = "https://api.nasa.gov/EPIC/api/natural/date/2021-12-13"
+    # get_image_url(url)            # работает
+    # fetch_spacex_last_launch(url) # работает
+    # get_url_extension(apod_url)     # работает125
+    # get_apod(apod_url)               # работает
+    # get_apod_photo(apod_url)           # работает
+    # get_epic_link(epic_link)                  # работает
+    # get_epic_photo(epic_link)        # работает
 
 if __name__ == "__main__":
     main()
