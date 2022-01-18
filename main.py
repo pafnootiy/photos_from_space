5d6a6ba82528d5c4f37bccc8bb9d6a2b788cd92a
pafnootiy <ksander-86@mail.ru>
2022-01-18 21:17:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_image_url(url):
+def get_flight_number_url(url, flight_number):
     payload = {
-        'flight_number': "107"
+        'flight_number': flight_number
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -15,35 +15,30 @@ def get_image_url(url):
     return response[0]['links']['flickr_images']
 
 
-def fetch_spacex_last_launch(url):
-    Path('photos_from_space/images').mkdir(parents=True, exist_ok=True)
-    for numbers, link in enumerate(get_image_url(url)):
-        filename = Path("images", f'spacex{numbers}.jpeg')
+def fetch_spacex_last_launch(url,flight_number,path_for_images_photos):
+    Path(path_for_images_photos).mkdir(parents=True, exist_ok=True)
+    for number, link in enumerate(get_flight_number_url(url, flight_number)):
+        filename = Path("images", f'spacex{number}.jpeg')
         response = requests.get(link)
         response.raise_for_status()
         with open(filename, 'wb') as file:
             file.write(response.content)
 
 
-
-
-def get_url_extension(apod_url):
-    api_key = os.getenv("API_KEY")
+def get_url_extension(apod_url,api_key):
     payload = {
         "api_key": api_key,
     }
     response = requests.get(apod_url, params=payload)
     extension = os.path.splitext(response.json()['hdurl'])
-    return print(extension[1])
+    return extension[1]
 
 
-def get_apod(apod_url):
-    api_key = os.getenv("API_KEY")
+def get_apod(apod_url, api_key):
     payload = {
         "api_key": api_key,
         "count": 15
     }
-
     response = requests.get(apod_url, params=payload)
     response = response.json()
     apod_photo_links = []
@@ -52,9 +47,9 @@ def get_apod(apod_url):
     return apod_photo_links
 
 
-def get_apod_photo(apod_url):
-    Path('photos_from_space/apod_pics').mkdir(parents=True, exist_ok=True)
-    for numbers, link in enumerate(get_apod(apod_url)):
+def download_apod_photo(apod_url,api_key,path_for_apod_photos):
+    Path(path_for_apod_photos).mkdir(parents=True, exist_ok=True)
+    for numbers, link in enumerate(get_apod(apod_url,api_key)):
         filename = Path("apod_pics", f'apod_pics{numbers}.jpeg')
         response = requests.get(link)
         response.raise_for_status()
@@ -62,8 +57,7 @@ def get_apod_photo(apod_url):
             file.write(response.content)
 
 
-def get_epic_link(epic_link):
-    api_key = os.getenv("API_KEY")
+def get_epic_link(epic_link,api_key):
     payload = {
         "api_key": api_key
     }
@@ -72,25 +66,22 @@ def get_epic_link(epic_link):
     date_link = response[0]["date"]
     image_name = response[0]["image"]
     date = datetime.datetime.fromisoformat(date_link)
-    epic_photo_link = f"https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/{date.day}/png/{image_name}.png"
-    epic_photo_link_list = []
-
+    epic_photo_links = []
     for image in response:
         epic_photo_link = f"https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/" \
                               f"{date.day}/png/{image['image']}.png"
-        epic_photo_link_list.append(epic_photo_link)
-    return epic_photo_link_list
+        epic_photo_links.append(epic_photo_link)
+    return epic_photo_links
 
 
-def get_epic_photo(epic_link):
-    api_key = os.getenv("API_KEY")
-    Path('photos_from_space/epic_pics').mkdir(parents=True, exist_ok=True)
-    for number, links in enumerate(get_epic_link(epic_link)):
+def download_epic_photo(epic_link,api_key,path_for_epic_photos):
+    Path(path_for_epic_photos).mkdir(parents=True, exist_ok=True)
+    for number, links in enumerate(get_epic_link(epic_link,api_key)):
         filename = Path("epic_pics", f'epic_pics{number}.jpeg')
         payload = {
             "api_key": api_key
         }
-        response = requests.get(links,params=payload)
+        response = requests.get(links, params=payload)
         response.raise_for_status()
         with open(filename, 'wb') as file:
             file.write(response.content)
@@ -98,16 +89,18 @@ def get_epic_photo(epic_link):
 
 def main():
     load_dotenv()
+    api_key = os.getenv("API_KEY")
     url = 'https://api.spacexdata.com/v3/launches/'
+    flight_number = 107
     apod_url = "https://api.nasa.gov/planetary/apod"
     epic_link = "https://api.nasa.gov/EPIC/api/natural/date/2021-12-13"
-    get_image_url(url)
-    fetch_spacex_last_launch(url)
-    get_url_extension(apod_url)
-    get_apod(apod_url)
-    get_apod_photo(apod_url)
-    get_epic_link(epic_link)
-    get_epic_photo(epic_link)
+    path_for_images_photos = "photos_from_space/images"
+    path_for_apod_photos = "photos_from_space/apod_pics"
+    path_for_epic_photos = 'photos_from_space/epic_pics'
+    fetch_spacex_last_launch(url,flight_number,path_for_images_photos)
+    download_apod_photo(apod_url, api_key, path_for_apod_photos)
+    download_epic_photo(epic_link, api_key,path_for_epic_photos)
+
 
 if __name__ == "__main__":
     main()
