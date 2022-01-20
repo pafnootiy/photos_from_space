780015aaec0b1cf2113222a63d382f9a1dd67710
pafnootiy <ksander-86@mail.ru>
2022-01-20 16:48:59 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -7,38 +7,26 @@ from os.path import join as joinpath
 from dotenv import load_dotenv
 
 
-def send_photo_in_tg(filepath):
-    with open(filepath, 'rb') as file:
-        image_file = file.read()
-    return image_file
+def post_photo_in_tg(folder, chat_id, bot):
+    path_to_folder = folder
+    for photo in listdir(path_to_folder):
+        if isfile(joinpath(path_to_folder, photo)):
+            filepath = f'{folder}/{photo}'
+            with open(filepath, 'rb') as file:
+                image_file = file.read()
+                bot.send_document(chat_id=chat_id, document=image_file)
 
 
 def main():
-
     load_dotenv()
-
+    folders = ["apod_pics", "images", "epic_pics"]
     bot = telegram.Bot(token=os.getenv("TG_TOKEN"))
     chat_id = os.getenv("CHAT_ID")
     bot_time = os.getenv("TIME")
     while True:
-
-        apod_path = "apod_pics"
-        for photo in listdir(apod_path):
-            if isfile(joinpath(apod_path, photo)):
-                filepath = f'apod_pics/{photo}'
-                bot.send_document(chat_id=chat_id, document=send_photo_in_tg(filepath))
-        image_path = "images"
-
-        for photo in listdir(image_path):
-            if isfile(joinpath(image_path, photo)):
-                filepath = f'images/{photo}'
-                bot.send_document(chat_id=chat_id, document=send_photo_in_tg(filepath))
-
-        epic_path = "epic_pics"
-        for photo in listdir(epic_path):
-            if isfile(joinpath(epic_path, photo)):
-                filepath = f'epic_pics/{photo}'
-                bot.send_document(chat_id=chat_id, document=send_photo_in_tg(filepath))
+        for folder in folders:
+            print(folder)
+            post_photo_in_tg(folder, chat_id, bot)
         time.sleep(int(bot_time))
 
 
